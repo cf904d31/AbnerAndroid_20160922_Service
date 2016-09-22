4fd68c6e8e6681de02af3e70a1260edd8733de62
cf904d31 <cf904d31@gmail.com>
2016-09-22 15:54:55 +0800

    Server介紹(撥放音樂得到長度)

--- app/src/main/java/iii/org/tw/server_background/MainActivity.java
+++ app/src/main/java/iii/org/tw/server_background/MainActivity.java
@@ -1,16 +1,25 @@
 package iii.org.tw.server_background;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
-
+    private MyReceiver receiver;
+    private SeekBar seekBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+        receiver = new MyReceiver();
+        IntentFilter filter = new IntentFilter("bradmp3");
+        registerReceiver(receiver,filter);
     }
 
     public void test1(View v) {
@@ -25,5 +34,13 @@ public class MainActivity extends AppCompatActivity {
 
     public void test3(View v) {
 
+    }
+
+    private class MyReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int len = intent.getIntExtra("len",-1);
+            seekBar.setMax(len);
+        }
     }
 }
