d5af6a80709a408b57f7c73b03ffacbbab194c3a
cf904d31 <cf904d31@gmail.com>
2016-09-22 15:39:09 +0800

    Server介紹(撥放音樂)

--- app/src/main/java/iii/org/tw/server_background/MyService.java
+++ app/src/main/java/iii/org/tw/server_background/MyService.java
@@ -2,10 +2,13 @@ package iii.org.tw.server_background;
 
 import android.app.Service;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.IBinder;
 import android.util.Log;
 
 public class MyService extends Service {
+
+    private MediaPlayer mp;
     public MyService() {
     }
 
@@ -17,19 +20,34 @@ public class MyService extends Service {
 
     @Override
     public void onCreate() {
-        Log.d("Abner","onCreate");
+        //Log.d("Abner","onCreate");
         super.onCreate();
+
+        mp = MediaPlayer.create(this,R.raw.littlt_lucky);
+
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Log.d("Abner","onStartCommand");
+        //Log.d("Abner","onStartCommand");
+        mp.start();
         return super.onStartCommand(intent, flags, startId);
+
+
     }
 
     @Override
     public void onDestroy() {
-        Log.d("Abner","onDestroy");
+        //Log.d("Abner","onDestroy");
+
         super.onDestroy();
+
+        if (mp != null) {
+            if(mp.isPlaying()) {
+                mp.stop();
+            }
+            mp.release();
+            mp = null;
+        }
     }
 }
